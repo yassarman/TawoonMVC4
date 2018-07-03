8bf113a8b9504ef8ad42da058e66db1f80fa5e2d
yassar <yfman1234@gmail.com>
2018-07-03 15:55:08 +0300

    first Commit

---
+++ TaawonMVC.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,205 @@
+﻿using System.Web.Optimization;
+
+namespace TaawonMVC.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(
+                new StyleBundle("~/Bundles/account-vendor/css")
+                    .Include("~/fonts/roboto/roboto.css", new CssRewriteUrlTransform())
+                    .Include("~/fonts/material-icons/materialicons.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/bootstrap/dist/css/bootstrap.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/toastr/toastr.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/famfamfam-flags/dist/sprite/famfamfam-flags.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/font-awesome/css/font-awesome.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/Waves/dist/waves.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/animate.css/animate.css", new CssRewriteUrlTransform())
+                    .Include("~/css/materialize.css", new CssRewriteUrlTransform())
+                    .Include("~/css/style.css", new CssRewriteUrlTransform())
+                    .Include("~/Views/Account/_Layout.css", new CssRewriteUrlTransform())
+            );
+
+            bundles.Add(
+                new ScriptBundle("~/Bundles/account-vendor/js/bottom")
+                    .Include(
+                        "~/lib/json2/json2.js",
+                        "~/lib/jquery/dist/jquery.js",
+                        "~/lib/bootstrap/dist/js/bootstrap.js",
+                        "~/lib/moment/min/moment-with-locales.js",
+                        "~/lib/jquery-validation/dist/jquery.validate.js",
+                        "~/lib/blockUI/jquery.blockUI.js",
+                        "~/lib/toastr/toastr.js",
+                        "~/lib/sweetalert/dist/sweetalert.min.js",
+                        "~/lib/spin.js/spin.js",
+                        "~/lib/spin.js/jquery.spin.js",
+                        "~/lib/Waves/dist/waves.js",
+                        "~/Abp/Framework/scripts/abp.js",
+                        "~/Abp/Framework/scripts/libs/abp.jquery.js",
+                        "~/Abp/Framework/scripts/libs/abp.toastr.js",
+                        "~/Abp/Framework/scripts/libs/abp.blockUI.js",
+                        "~/Abp/Framework/scripts/libs/abp.spin.js",
+                        "~/Abp/Framework/scripts/libs/abp.sweet-alert.js",
+                        "~/js/admin.js",
+                        "~/js/main.js"
+                    )
+            );
+
+            //VENDOR RESOURCES
+
+            //~/Bundles/vendor/css
+            bundles.Add(
+                new StyleBundle("~/Bundles/vendor/css")
+                .Include("~/fonts/roboto/roboto.css", new CssRewriteUrlTransform())
+                .Include("~/fonts/material-icons/materialicons.css", new CssRewriteUrlTransform())
+                .Include("~/lib/bootstrap/dist/css/bootstrap.css", new CssRewriteUrlTransform())
+                .Include("~/lib/bootstrap-select/dist/css/bootstrap-select.css", new CssRewriteUrlTransform())
+                .Include("~/lib/toastr/toastr.css", new CssRewriteUrlTransform())
+                .Include("~/lib/famfamfam-flags/dist/sprite/famfamfam-flags.css", new CssRewriteUrlTransform())
+                .Include("~/lib/font-awesome/css/font-awesome.css", new CssRewriteUrlTransform())
+                .Include("~/lib/Waves/dist/waves.css", new CssRewriteUrlTransform())
+                .Include("~/lib/animate.css/animate.css", new CssRewriteUrlTransform())
+                .Include("~/css/materialize.css", new CssRewriteUrlTransform())
+                .Include("~/css/style.css", new CssRewriteUrlTransform())
+                .Include("~/css/themes/all-themes.css", new CssRewriteUrlTransform())
+                .Include("~/Views/Shared/_Layout.css", new CssRewriteUrlTransform())
+            );
+
+            //~/Bundles/vendor/bottom (Included in the bottom for fast page load)
+            bundles.Add(
+                new ScriptBundle("~/Bundles/vendor/js/bottom")
+                    .Include(
+                        "~/lib/json2/json2.js",
+                        "~/lib/jquery/dist/jquery.js",
+                        "~/lib/bootstrap/dist/js/bootstrap.js",
+                        "~/lib/moment/min/moment-with-locales.js",
+                        "~/lib/jquery-validation/dist/jquery.validate.js",
+                        "~/lib/blockUI/jquery.blockUI.js",
+                        "~/lib/toastr/toastr.js",
+                        "~/lib/sweetalert/dist/sweetalert.min.js",
+                        "~/lib/spin.js/spin.js",
+                        "~/lib/spin.js/jquery.spin.js",
+                        "~/lib/bootstrap-select/dist/js/bootstrap-select.js",
+                        "~/lib/jquery-slimscroll/jquery.slimscroll.js",
+                        "~/lib/Waves/dist/waves.js",
+                        "~/lib/push.js/push.js",
+                        "~/Abp/Framework/scripts/abp.js",
+                        "~/Abp/Framework/scripts/libs/abp.jquery.js",
+                        "~/Abp/Framework/scripts/libs/abp.toastr.js",
+                        "~/Abp/Framework/scripts/libs/abp.blockUI.js",
+                        "~/Abp/Framework/scripts/libs/abp.spin.js",
+                        "~/Abp/Framework/scripts/libs/abp.sweet-alert.js",
+                        "~/js/admin.js",
+                        "~/js/main.js",
+                        "~/Scripts/jquery.signalR-2.2.3.js",
+                        "~/Views/Shared/_Layout.js"
+                    )
+                );
+
+            //Home-Index Bundles
+            bundles.Add(
+                new ScriptBundle("~/Bundles/home-index")
+                    .Include(
+                        "~/lib/jquery-countTo/jquery.countTo.js",
+                        "~/lib/raphael/raphael.js",
+                        "~/lib/morris.js/morris.js",
+                        "~/lib/chart.js/dist/Chart.bundle.js",
+                        "~/lib/Flot/jquery.flot.js",
+                        "~/lib/Flot/jquery.flot.resize.js",
+                        "~/lib/Flot/jquery.flot.pie.js",
+                        "~/lib/Flot/jquery.flot.categories.js",
+                        "~/lib/Flot/jquery.flot.time.js",
+                        "~/lib/jquery-sparkline/dist/jquery.sparkline.js",
+                        "~/Views/Home/Index.js"
+                    )
+            );
+
+            //APPLICATION RESOURCES
+
+            //~/Bundles/css
+            bundles.Add(
+                new StyleBundle("~/Bundles/css")
+                    .Include("~/css/main.css")
+                );
+
+            //bundles for files upload 
+            //===========================
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
+
+            //-----------------------------
+
+            bundles.Add(new StyleBundle("~/Content/jQuery-File-Upload").Include(
+                   "~/Content/jQuery.FileUpload/css/jquery.fileupload.css",
+                  "~/Content/jQuery.FileUpload/css/jquery.fileupload-ui.css",
+                  "~/Content/blueimp-gallery2/css/blueimp-gallery.css",
+                    "~/Content/blueimp-gallery2/css/blueimp-gallery-video.css",
+                      "~/Content/blueimp-gallery2/css/blueimp-gallery-indicator.css"
+                  ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jQuery-File-Upload").Include(
+                     //<!-- The Templates plugin is included to render the upload/download listings -->
+                     "~/Scripts/jQuery.FileUpload/vendor/jquery.ui.widget.js",
+                       "~/Scripts/jQuery.FileUpload/tmpl.min.js",
+//<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+"~/Scripts/jQuery.FileUpload/load-image.all.min.js",
+//<!-- The Canvas to Blob plugin is included for image resizing functionality -->
+"~/Scripts/jQuery.FileUpload/canvas-to-blob.min.js",
+//"~/Scripts/file-upload/jquery.blueimp-gallery.min.js",
+//<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+"~/Scripts/jQuery.FileUpload/jquery.iframe-transport.js",
+//<!-- The basic File Upload plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload.js",
+//<!-- The File Upload processing plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-process.js",
+//<!-- The File Upload image preview & resize plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-image.js",
+//<!-- The File Upload audio preview plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-audio.js",
+//<!-- The File Upload video preview plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-video.js",
+//<!-- The File Upload validation plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-validate.js",
+//!-- The File Upload user interface plugin -->
+"~/Scripts/jQuery.FileUpload/jquery.fileupload-ui.js",
+//Blueimp Gallery 2 
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery.js",
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery-video.js",
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery-indicator.js",
+"~/Scripts/blueimp-gallery2/js/jquery.blueimp-gallery.js"
+
+));
+
+            bundles.Add(new ScriptBundle("~/bundles/Blueimp-Gallerry2").Include(//Blueimp Gallery 2 
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery.js",
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery-video.js",
+"~/Scripts/blueimp-gallery2/js/blueimp-gallery-indicator.js",
+"~/Scripts/blueimp-gallery2/js/jquery.blueimp-gallery.js"));
+
+            //==========================
+        }
+    }
+}
---
+++ TaawonMVC.Web/Models/FilesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TaawonMVC.Web.Helpers;
+
+namespace TaawonMVC.Web.Models
+{
+    public class FilesViewModel
+    {
+        public ViewDataUploadFilesResult[] Files { get; set; }
+    }
+}
